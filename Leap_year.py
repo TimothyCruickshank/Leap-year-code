cb3ebed60507e447c5f17216750f02c70958a046
TimothyCruickshank <111780723+TimothyCruickshank@users.noreply.github.com>
2022-11-30 11:23:52 -0500

    leap year code

--- Leap_year.py
+++ Leap_year.py
@@ -1,10 +1,17 @@
+year = int(input("Enter a year to check if it's a leap year or not? "))
 
-year = int(input("What is the year? "))
-if (year % 4) == 0:
-    if (year % 100) == 0:
-     if (year % 400) == 0:
-         print ("Leap year.")
-     else:
-        print("Not leap year.")
+# Divisible by 4 - leap year
+# Exception is if it's divisble by 400 - leap year
+# BUT exception if it's divisible by 400 - leap year
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap Year")
+        else:
+            print("Not a Leap Year")
+    else:
+        print("leap Year")
 else:
-    print("Not leap year.")
+    print("Not a Leap Year")
+    
